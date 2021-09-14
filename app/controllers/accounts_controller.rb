ff29d8c14dd634ba051e0740edcaf4422c2acdb7
Tyler Case <tcase629@yahoo.com>
2021-09-13 21:06:45 -0600

    flash messages

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,14 +1,54 @@
 class AccountsController < ApplicationController
   def index
-    render component: "Accounts"
+    @accounts = current_user.accounts
+    render component: "Accounts", props: { accounts: @accounts, user: current_user}
   end
 
   def show
+    @account = current_user.accounts.find(params[:id])
+    render component: "Account", props: { account: @account, user: current_user}
   end
 
   def new
+    @account = current_user.accounts.new
+    render component: "AccountNew", props: { account: @account, user: current_user}
+  end
+
+  def create
+    if @account.save
+      flash[:success] = "Account Created"
+      redirect_to root_path
+    else
+      flash[:error] = "Error #{@accounts.errors.full_messages.join("\n")}"
+      render component: "AccountNew", props: { account: @account, user: current_user}
+    end
   end
 
   def edit
+    @account = current_user.accounts.find(params[:id])
+    render component: "AccountEdit", props: { account: @account, user: current_user}
+  end
+
+  def update
+    @account = current_user.accounts.find(params[:id])
+    if @account.update(account_params)
+      flash[:success] = "Account Updated"
+      redirect_to root_path
+    else
+      flash[:error] = "Error #{@accounts.errors.full_messages.join("\n")}"
+      render component: "AccountEdit", props: { account: @account, user: current_user}
+    end
+  end
+
+  def destroy
+    @account = current_user.accounts.find(params[:id])
+    @account.destroy
+    flash[:success] = "Account Deleted"
+    redirect_to root_path
+  end
+
+  private
+  def account_params
+    params.require(:account).permit(:account_name, :balance)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected 
+  # model_params
+    # params.require(:sign_up).permit(:email, :password)
+    # params.require(:sign_up).permit(:email, :password, :first_name, :last_name)
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
 end
